ef6ca22a6cf8fa2445f98b82d3112cafb2de4060
Dennis Chen <chendennis1998@gmail.com>
2021-03-01 16:32:19 +1100

    Added installation of various things

--- install-everything.sh
+++ install-everything.sh
@@ -2,34 +2,41 @@
 CFG_DIR=$HOME/config-setup
 
 # install VSCode
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+if command -v code; then
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code 
+    sudo apt install -y apt-transport-https
+    sudo apt update
+    sudo apt install -y code 
+fi 
 
 # install logid
-sudo apt install cmake libevdev-dev libudev-dev libconfig++-dev
-cd /tmp
-git clone https://github.com/PixlOne/logiops
-cd logiops
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
+if command -v logid; then
+    sudo apt install -y cmake libevdev-dev libudev-dev libconfig++-dev
+    cd /tmp
+    git clone https://github.com/PixlOne/logiops
+    cd logiops
+    mkdir build
+    cd build
+    cmake ..
+    make
+    sudo make install
+fi
 
 # setup Vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install ohmyzsh
-cd /tmp
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
-# install required fonts
-sudo apt-get install fonts-powerline
+if command -v zsh; then
+    cd /tmp
+    wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+    sh install.sh
+    # install required fonts
+    sudo apt install -y fonts-powerline
+    sudo apt install -y autojump 
+fi
 
 # setup the symbolic links
 sh $CFG_DIR/setup_links.sh
@@ -37,3 +44,11 @@ sh $CFG_DIR/setup_links.sh
 # install required vim plugins
 vim +PluginInstall +qall
 
+# install expect modules for ansible
+#sudo apt install -y python3-pexpect
+#sudo apt install -y expect
+
+if command -v conda; then
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.zsh
+fi
--- setup_links.sh
+++ setup_links.sh
@@ -5,7 +5,7 @@ ln -sf $CFG_DIR/.vimrc .vimrc
 ln -sf $CFG_DIR/.zshrc .zshrc
 sudo ln -sf $CFG_DIR/logid.cfg /etc/logid.cfg
 
-if test -d "$HOME/.config/Code/User"; then 
+if [ -d "$HOME/.config/Code/User" ]; then 
     ln -sf $CFG_DIR/vscode-settings.json .config/Code/User/settings.json 
 else
     echo "vscode not installed"
